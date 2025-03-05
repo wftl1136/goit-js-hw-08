fba460a013687a0b7e2cb3fc553d823bda773ca3
Maksym <zzzeleznyakmaxim@gmail.com>
2025-03-05 20:00:46 +0200

    full

--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const textInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value.trim()||"Anonymous";
-});
